baca8a3be81f617d758dc54b0c83b971b94bc848
napakornsk <napakorn.sk@gmail.com>
2026-02-24 02:20:47 +0700

    feat: add prod branch

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:8000",
+        target: process.env.VITE_API_URL || "https://hsi.napakornsk.xyz",
         changeOrigin: true,
       },
     },
